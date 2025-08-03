2f1f383b0ab2df7cf5ffb4031ae7f6d2d80b92e8
Kutub Udd <socialnetwork98710@gmail.com>
2025-08-03 19:26:48 +0600

    added

--- .next/static/chunks/src_app_(main)_layout_tsx_fd15e2b3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0b29103d._.js",
-  "static/chunks/src_a7ceeebb._.js"
-],
-    source: "dynamic"
-});
