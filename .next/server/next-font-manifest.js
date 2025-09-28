d52f4070202c7dbf68dc20b5d12a8e147df89f33
musfiqurofficial <musfiqurofficial1999@gmail.com>
2025-09-28 21:28:21 +0600

    new code

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
+self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/src/app/(all-dashboard)/admin/dashboard/page\": [],\n    \"[project]/src/app/(main)/[eventId]/page\": [],\n    \"[project]/src/app/(main)/page\": [],\n    \"[project]/src/app/admin/login/page\": []\n  },\n  \"appUsingSizeAdjust\": false,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
